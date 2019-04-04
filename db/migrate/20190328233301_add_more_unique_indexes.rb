584d8bfe913b197b367558d1bf89b692eb6d1c04
Dean Hatch <dean.hatch@milehighsoftware.com>
2019-04-03 21:40:36 -0400

    Replaced ValidTime model

--- db/migrate/20190328233301_add_more_unique_indexes.rb
+++
@@ -1,8 +0,0 @@
-class AddMoreUniqueIndexes < ActiveRecord::Migration[5.1]
-  def change
-    
-    change_column :valid_times, :venue_id, :bigint
-    
-    change_column :valid_dates, :competition_id, :bigint
-  end
-end
