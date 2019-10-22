47d5401ab8f8d5512d9fc79203ce3993d287daf0
Dean Hatch <dean.hatch@milehighsoftware.com>
2019-10-22 09:05:16 -0400

    Start Versionizing Routes and Controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,36 +1,48 @@
 Rails.application.routes.draw do
-  
-  resources :organizations do
-    resources :competitions, shallow: true do
-      resources :groupings, shallow: true do
-	resources :teams, shallow: true
-      end  # end of groupings resources
-      resources :venues
-      resources :valid_dates
-      resources :valid_times
-    end  # end of competitions resources
-  end
 
-    # Non-nested route to handle case where all Competitions
-    # across all Organizations are requested
-  get 'competitions',
+  concern :api do
+      resources :organizations do
+	resources :competitions, shallow: true do
+	  resources :groupings, shallow: true do
+	    resources :teams, shallow: true do
+	      resources :alert_requests, shallow: true 
+	    end # of teams resources
+	  end  # end of groupings resources
+	  resources :contests
+	  resources :regularcontests
+	  resources :bracketcontests
+	  resources :contestants, only: [:index, :show]
+	  resources :venues
+	  resources :valid_dates
+	  resources :valid_times
+	end  # end of competitions resources
+      end  # of organizations resources
+      
+      # Non-nested route to handle case where all Competitions
+      # across all Organizations are requested
+      get 'competitions',
           to: 'competitions#index_across_organizations', 
-	  as: :competitions_across_organizations
-
-  #get 'competitions/:competition_id/teams',
-  #        to: 'teams#index_for_competition', 
-#	  as: :teams_for_competition
+         as: :competitions_across_organizations
+     end  # of api concern
+    
+    namespace :v0 do
+      concerns :api
+      end  # of v0 namespace
+     
+       #get 'competitions/:competition_id/teams',
+       #        to: 'teams#index_for_competition', 
+       #	  as: :teams_for_competition
+       
   
-  
-  resources :contests
-  resources :contestants
   resources :bcadvancements
   resources :groupingplaces
-  resources :credits
-  resources :alert_requests
   
   devise_for :customers, controllers: {sessions: 'customers/sessions' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :customers do
+    resources :credits
+    end
   
-  root to: "home#index"
+  #root to: "home#index"
+  root to: "application#index"
 end
